0d4a6451a63369916020d0e80e294059068653f3
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-23 12:13:06 +0100

    a puppet manifest to kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,7 @@
 # A Puppet manifest to kill a process
 
 exec { 'killmenow':
-  command     => 'pkill -f "killmenow"',
-  path        => ['/usr/bin', '/bin'],      # Adjust the path as needed
-  onlyif      => 'pgrep -f "killmenow"',    # Check if the process exists before attempting to kill it
-  refreshonly => true,                  # Only run when notified
+  command => '/usr/bin/pkill killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow',
 }
 
