0fa2c9f27f1d77d028fb100931bc4f71768016b5
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-23 23:39:48 +0100

    automation with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Automating configuration using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+content  => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
