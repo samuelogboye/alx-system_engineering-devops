0ac33511265a0cf6b10574cc80115d4c18f3df4e
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-23 11:53:17 +0100

    creating a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# Create a file at /tmp/school with specific attributes
+
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  ensure  => file,                  # Ensure it's a regular file
+  mode    => '0744',                # Set file permissions to 0744
+  owner   => 'www-data',            # Set the owner to www-data
+  group   => 'www-data',            # Set the group to www-data
+  content => 'I love Puppet',       # Set the file content
 }
