def8f8b2382b5d244868954d5e2bb8858a7ebcf5
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-24 01:48:44 +0100

    updated to json2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,23 +19,17 @@ if __name__ == "__main__":
     with urllib.request.urlopen(todoUrl) as response:
         data = json.loads(response.read().decode())
         tasks = data
-        done = 0
-        done_tasks = []
 
-        for task in tasks:
-            if task.get('completed'):
-                done_tasks.append(task)
-                done += 1
 
-        with open('{}.json'.format(employeeId), mode='w', newline='') as file:
-            json.dump(
-                {
-                    employeeId: [
-                        {
-                            "task": task.get('title'),
-                            "completed": task.get('completed'),
-                            "username": employeeName
-                        } for task in tasks
-                    ]
-                },
-                file)
+    with open('{}.json'.format(employeeId), mode='w', newline='') as file:
+        json.dump(
+            {
+                employeeId: [
+                    {
+                        "task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": employeeName
+                    } for task in tasks
+                ]
+            },
+            file)
