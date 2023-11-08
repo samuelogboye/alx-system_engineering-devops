e6a8d99723a037b8a5494e850d21abeeacf0c030
Samuel Ogboye <ogboyesam@gmail.com>
2023-11-08 04:55:06 +0100

    updated more

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,18 +6,19 @@ import requests
 
 
 def top_ten(subreddit):
-        """ A function that prints the titles of the first 10 hot posts listed for
-                a given subreddit.
-                """
+        """
+        A function that prints the titles of the first
+        10 hot posts listed for a given subreddit.
+        """
         url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
         headers = {'User-Agent': 'My User Agent 1.0'}
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-                count = 1
-                for post in response.json().get('data').get('children'):
-                        print(post.get('data').get('title'))
-                        count += 1
-                        if count > 10:
-                                break
-        else:
-                print('None')
+        params = {
+              "limit": 10
+        }
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        if response.status_code == 404:
+            print("None")
+            return
+        results = response.json().get("data")
+        [print(c.get("data").get("title")) for c in results.get("children")]
