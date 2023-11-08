193ef205d3f0a0d5473200a9b2ee07c5d5c7eec7
Samuel Ogboye <ogboyesam@gmail.com>
2023-11-08 05:15:28 +0100

    more updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-""" A recursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit. If no
-    results are found for the given subreddit, the function should return None.
-    """
+"""
+Using reddit's API
+"""
 import requests
-
-
 after = None
+
+
 def recurse(subreddit, hot_list=[]):
-    """ A recursive function that returns a list containing
-    the titles of all hot articles for a given subreddit.
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    params = {
-              "after": after
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        return None
-    results = response.json().get("data")
-    after = results.get("after")
-    if after is None:
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
-        hot_list += [c.get("data").get("title")
-                     for c in results.get("children")]
-        return recurse(subreddit, hot_list, after)
+        return (None)
